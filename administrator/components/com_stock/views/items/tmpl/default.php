eddeee67a43f4d70a3b38b8baea3b02c0a76850a
bblurock <llwbenson@gmail.com>
2014-04-09 02:42:38 +0800

    Update items list view

--- administrator/components/com_stock/views/items/tmpl/default.php
+++ administrator/components/com_stock/views/items/tmpl/default.php
@@ -6,10 +6,36 @@
 	method="post"
 	name="adminForm"
 	id="adminForm">
-	<?php
-	var_dump($this->items);
-	?>
-	<input type="hidden" name="option" value="com_stock"/>
-	<input type="hidden" name="task" value=""/>
+	<table class="table table-striped">
+		<thead>
+		<tr>
+			<th>
+				ID
+			</th>
+			<th>
+				Stock Number
+			</th>
+		</tr>
+		</thead>
+		<tbody>
+		<?php foreach ($this->items as $i => $item): ?>
+			<tr>
+				<td>
+					<a href="<?php echo JRoute::_('index.php?option=com_stock&task=item.edit&id=' . $item->id); ?>">
+						<?php echo $this->escape($item->id); ?>
+					</a>
+				</td>
+				<td>
+					<?php echo $item->stock_number; ?>
+				</td>
+			</tr>
+		<?php
+		endforeach;
+		?>
+		</tbody>
+	</table>
+
+	<input type="hidden" name="option" value="com_stock" />
+	<input type="hidden" name="task" value="" />
 	<?php echo JHtml::_('form.token'); ?>
 </form>
