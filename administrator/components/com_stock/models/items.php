10755836908d7cafe3461269bfa36ee9c3ff13d7
bblurock <llwbenson@gmail.com>
2014-04-08 22:30:42 +0800

    建立models

---
+++ administrator/components/com_stock/models/items.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class StockModelItems
+ */
+class StockModelItems extends JModelList
+{
+	/**
+	 * Method to get a JDatabaseQuery object for retrieving the data set from a database.
+	 *
+	 * @return JDatabaseQuery
+	 */
+	public function getListQuery()
+	{
+		$db    = $this->getDbo();
+		$query = $db->getQuery(true);
+
+		$query->select('stock.*')
+				->from('#__stock_items AS stock');
+
+		var_dump($query);
+
+		return $query;
+	}
+}
